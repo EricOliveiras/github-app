0fe974c9a2cd9157e671a0369fb336a5f8f4bbf9
Eric Oliveira <oiericoi@hotmail.com>
2022-07-18 15:40:04 -0300

    adicionado alguuns componentes e estilos

---
+++ Readme.md
@@ -0,0 +1,1 @@
+Projeto Githb App
---
+++ src/components/profile/styled.js
@@ -0,0 +1,33 @@
+import styled from 'styled-components'
+
+export const Wrapper = styled.div`
+  display: flex;
+  align-items: flex-start;
+  width: 100wh;
+`
+export const WrapperStatsCount = styled.div`
+  display: flex;
+
+  div {
+    margin-right: 10px;
+  }
+`
+export const WrapperUserName = styled.div`
+  display: flex;
+  align-items: center;
+
+  h3 {
+    margin-right: 10px;
+  }
+`
+export const WrapperInfoUser = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-left: 10px;
+`
+export const WrapperImgUser = styled.img`
+  border-radius: 50%;
+  width: 200px;
+  height: 200px;
+  margin: 0 10px;
+`
